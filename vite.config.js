9ca0b20188a5e431f67449b25d8c9912b2f3442f
Soumit <soumitdasgupta2021@gmail.com>
2026-08-01 22:26:28 +0530

    Update base path for QR File Transfer service

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/QR-FILE_TRANSFER/',
+  base: '/QR-File-Transfer-Service/',
   plugins: [
     react(),
     VitePWA({ 
